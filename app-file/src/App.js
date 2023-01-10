9489ea6772c64132856d13c016133c8b2015ee45
neoArio <fazelfallahb11@gmail.com>
2023-01-10 14:41:01 +0330

    raw react app added

---
+++ app-file/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+
+    return (
+        <h1>raw food order application</h1>
+        <p>after each step and major changes i commit and track every chenges in repository</p>
+        <p>hope u guys find this project useful</p>
+        
+        )
+}
+
